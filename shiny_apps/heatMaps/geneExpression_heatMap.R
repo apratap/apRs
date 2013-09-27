83f8cbb6dfb1acb1d4769baa0084a03eee431651
apratap <abhishek.vit@gmail.com>
2013-09-27 13:13:47 -0700

    v3

--- shiny_apps/heatMaps/geneExpression_heatMap.R
+++ shiny_apps/heatMaps/geneExpression_heatMap.R
@@ -1,10 +1,9 @@
-require("Heatplus")
+#require("Heatplus")
+require("pheatmap")
 
 
 #gene expression heatmap logic
-
-
-get_geneExpression_heatMap <- function(m,...){
+get_geneExpression_heatMap <- function(m,annotation,...){
   
   #change the data type to integer
   m <- apply(m,2,as.numeric)
@@ -14,21 +13,40 @@ get_geneExpression_heatMap <- function(m,...){
   drop_genes <- which(apply(m,1,sd) < .2)
   m <-  m[-drop_genes,]
   
-  
   #scaling genes across experiments
   mat.scaled <- t(scale(t(m)))
   
-  #custom functions for heatmap plus
-  corrdist <- function(x) as.dist( 1 - cor(t(x),method="spearman"))
-  hclust.avl = function(x) hclust(x, method="average")
   
-  #v3
-  reg3 <- regHeatmap(mat.scaled,
-                     legend=2,
-                     dendrogram = list(clustfun=hclust.avl,
-                                       distfun = corrdist))
-#                      xlab = "PCBC Samples",
-#                      ylab = "Selected genes")
-  plot(reg3)
+  pheatmap(mat.scaled,
+           scale="none",
+           annotation = annotation,
+           clustering_distance_rows = "correlation",
+           clustering_distance_cols = "correlation",
+           clustering_method = "average",
+           fontsize_col = 4,
+           border_color = NA
+  )
 }
+  
+  
+  
+  #custom functions for heatmap plus
+#   corrdist <- function(x) as.dist( 1 - cor(t(x),method="spearman"))
+#   hclust.avl = function(x) hclust(x, method="average")
+#   
+  #v3
+#   reg3 <- regHeatmap(mat.scaled,
+#                      dendrogram = list(clustfun=hclust.avl,
+#                                        distfun = corrdist),
+#                      legend = FALSE
+#                      )
+#   
+#   par(mar=c(2,1,1,.5)+0.1,
+#       fin=c(10,10),
+#       pin=c(8,8))
+#   print(paste0('Margin',par()$mar))
+#   print(paste0('MGP', par()$mgp))
+#   print(paste0('FIN', par()$fin))
+#   print(paste0('PIN',par()$pin))
+#   plot(reg3)
   
