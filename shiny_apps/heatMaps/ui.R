bb585592fbf7078653228b6722dd67452a98c33b
apratap <abhishek.vit@gmail.com>
2013-10-09 17:26:43 -0700

    v3 changes

--- shiny_apps/heatMaps/ui.R
+++ shiny_apps/heatMaps/ui.R
@@ -8,54 +8,49 @@ shinyUI(pageWithSidebar(
 
   #SIDE BAR PANEL FOR USER OPTIONS
   sidebarPanel(
-#     selectInput("user_selected_pathway",
-#                 "Select a pathway:",
-#                 choices = names(KEGG_available_pathways) #loaded from getDATA.R
-#     ),
-#     br(),
-#   
+
     selectInput("user_selected_geneList",
                   "Significant gene lists",
                   choices = sort(names(precomputed_enrichedPathways_in_geneLists)) #loaded from getDATA.R
     ),
+       
+    br(),
     
-    #list of enriched pathways
-    #dynamically updated based on used selected gene list
-#     selectInput("enrichedPathways",
-#                 "Enriched pathway/s",
-#                 choices = 'select a significant gene list'
-#     ),
+    #dynamically updated
+    selectInput('enrichedPathways','Enriched Pathways',choices='ALL'),
+    
+    br(),
 
-    
-    br(),
-    br(),
-    
-    #dynamic UI
-    uiOutput("enrichedPathways"),
-    
-    br(),
+    #heatmap annotation labels
+    checkboxGroupInput('heatmap_annotation_labels', h5('Heatmap Annotation Labels'),
+                       choices  = names(heatmap_annotation_cols),
+                       selected = c('Diff Name')),
     br(),
     #FILTER OPTIONS
     h4('Custom Filters:'),
     
-    #1. filter based on group level 1 differentiation state
-    checkboxGroupInput('level_1_diff_state', h5('Level 1 differentiation state'),choices=level_1_diff_state),
+    #1. filter based on mod_linetype
+    checkboxGroupInput('mod_linetype', h5('Line Type'),choices=sort(unique(metadata$mod_linetype) ) ),
     br(),
     
-    #2. filter based on group level 3 differentiation state
-    checkboxGroupInput('level_3_diff_state',h5('Level 3 differentiation state'),choices=level_3_diff_state),
+    #2. filter based on diff_short_name
+    checkboxGroupInput('mod_diffnameshort',h5('Differentiation Name'),choices= sort(unique(metadata$mod_diffnameshort) ) ),
     br(),
     
     #3. filter based on cell origin
-    checkboxGroupInput('cell_origin',h5('Cell type of origin'),choices=cell_origin),
+    checkboxGroupInput('cell_origin',h5('Cell Origin'),choices=sort(unique(metadata$mod_origcell) ) ),
     br(),
     
+    #4. filter based on induction genes
+    checkboxGroupInput('induction_genes',h5('Induction Genes'),choices=sort(unique(metadata$inductiongenes) ) ),
+    br(),
     
     #. Filter based on user defined search list
     h5('Search on a custom gene list:'),
     tags$textarea(id="custom_gene_list",rows=5,cols=60,sample_gene_list),
     #helpText("Note: Select <Custom gene list> in the pathway list above"),
     
+    br(),
     #Download fitlered data
     downloadButton('downloadData','Download Filtered Exp Data')
   ),
@@ -63,6 +58,7 @@ shinyUI(pageWithSidebar(
   
   #Main shiny panel
   mainPanel(
+    textOutput("msg"),
     plotOutput("heatMap",height="700px"),
     br(),
     tableOutput("summary"),
