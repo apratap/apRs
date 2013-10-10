1c09a65fe9f2702c94c80a9445d86a49f6c923ef
apratap <abhishek.vit@gmail.com>
2013-10-09 17:35:15 -0700

    moved getData.R to global.R

--- shiny_apps/heatMaps/global.R
+++ shiny_apps/heatMaps/global.R
@@ -16,7 +16,7 @@ load(MSIGDB@filePath) #available as MSigDB R object
 ###
 #get the PCBC samples gene normalized counts
 ###
-syn_geneNormCounts <- synGet('syn1968267')
+syn_geneNormCounts <- synGet('syn2247799')
 #read in the file
 geneNormCounts <- read.table(syn_geneNormCounts@filePath,header=T,sep='\t')
 
@@ -30,7 +30,7 @@ sigGenes_lists <- readRDS("precomputed_sigGenes_lists.rds")
 ###
 #get the metadata from the synapse for PCBC samples
 ###
-METADATA_ID <- 'syn2024470'
+METADATA_ID <- 'syn2248030'
 query <- sprintf('select * from entity where parentId=="%s"', METADATA_ID)
 metadata <- synQuery(query)
 cols_to_be_deleted = c('entity.benefactorId', 'entity.concreteType', 'entity.createdByPrincipalId', 
@@ -45,21 +45,59 @@ metadata <- metadata[,!names(metadata) %in% cols_to_be_deleted]
 names(metadata) <- gsub('entity.','',names(metadata))
 
 
+########
+#create new higher order labels for heatmap labels
+#groups are merged into higher order
+########
+#1. linetype
+# merge everthing other than hESC , iPSC, None into tissue
+metadata$mod_linetype <-  metadata$linetype
+index_to_be_replaced <-  ! metadata$linetype %in% c('hESC','iPSC','None')
+metadata$mod_linetype[index_to_be_replaced] = 'tissue'
+
+
+#2. diffnameshort
+metadata$mod_diffnameshort <- metadata$diffnameshort
+#2A. EB-LF should be renamed EB and similarly SC-LF should be SC
+metadata$mod_diffnameshort <- sub('SC-.*','SC',metadata$mod_diffnameshort)
+metadata$mod_diffnameshort <- sub('EB-.*','EB',metadata$mod_diffnameshort)
+#2B. anything except the following should be grouped into Other
+# SC, EB, DE, ECTO, MESO, MESO-30, MESO-15
+index_to_be_replaced <- ! metadata$mod_diffnameshort %in% c('SC', 'EB', 'DE', 'ECTO', 'MESO', 'MESO-30', 'MESO-15')
+metadata$mod_diffnameshort[index_to_be_replaced] = 'Others'
+
+#3. origcell
+metadata$mod_origcell <- metadata$origcell
+#3A. categorize UCB CD34+ into CD34+
+metadata$mod_origcell <- sub('^.*CD34\\+$','CD34+',metadata$mod_origcell,perl=T)
+#3B. categ bone marrow to BM MSC
+metadata$mod_origcell <- sub('bone marrow','BM MSC',metadata$mod_origcell,perl=T)
+#3C. adultHeart to tissue
+metadata$mod_origcell <- sub('adultHeart','tissue',metadata$mod_origcell,perl=T)
+#3C. categ the following into Others
+Others_group <- c('embryo', 'primEndothel', 'skin', 'None')
+index_to_be_replaced <- metadata$mod_origcell %in% Others_group
+metadata$mod_origcell[index_to_be_replaced] <- 'Others'
+
+
+
+##########
+#create possible heatmap annotation labels
+##########
+heatmap_annotation_cols <- c('Gender'='donorsex','line type'='mod_linetype',
+                             'Diff Name'='mod_diffnameshort','Cell Origin'='mod_origcell',
+                             'Induction Genes' = 'inductiongenes')
+
+
+
+
+
+
 #1. sex
-sex <- unique(metadata$donorsex.cell.lines)
+sex <- unique(metadata$donorsex)
 sex <- sex[sex != "None"]
 
-#2. group level 1 diff state
-level_1_diff_state <- unique(metadata$grouplevel1differentiationstate)
-level_1_diff_state <- level_1_diff_state[ !level_1_diff_state %in% c('None','NA')]
 
-#3. group level 3 diff stat
-level_3_diff_state <- unique(metadata$grouplevel3differentiationstate)
-level_3_diff_state <- level_3_diff_state[ !level_3_diff_state %in% c('None','NA')]
-
-#4. 
-cell_origin <- unique(metadata$celltypeoforigin)
-cell_origin <- cell_origin[ !cell_origin %in% c('None','NA')]
 
 
 
@@ -90,14 +128,13 @@ sample_gene_list <- paste(sample_gene_list,",")
 df_precomputed_enrichedPathways_in_geneLists = readRDS("precomputed_enrichedPathways_in_geneLists.rds")
 
 df_precomputed_enrichedPathways_in_geneLists$pathways_with_pvalue =  paste(df_precomputed_enrichedPathways_in_geneLists$pathways,
-                                                                        '#p.adj_',
-                                                                        format.pval(df_precomputed_enrichedPathways_in_geneLists$p.adj,digits=2),
-                                                                        sep='')
+                                                                           '#p.adj_',
+                                                                           format.pval(df_precomputed_enrichedPathways_in_geneLists$p.adj,digits=2),
+                                                                           sep='')
 
 
 
-df_precomputed_enrichedPathways_in_geneLists$significant_gene_list_name 
-  
+
 #creating a list of list 
 precomputed_enrichedPathways_in_geneLists = split(df_precomputed_enrichedPathways_in_geneLists$pathways_with_pvalue,
                                                   df_precomputed_enrichedPathways_in_geneLists$significant_gene_list_name)
@@ -115,7 +152,6 @@ precomputed_enrichedPathways_in_geneLists <- lapply(precomputed_enrichedPathways
 
 #add place holder for custom gene list
 precomputed_enrichedPathways_in_geneLists[['Custom gene list']] = 'NA'
-
 
 
 
