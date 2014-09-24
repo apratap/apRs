16c4ba0f27c7accc8dd234e84e02b7479ab10f4e
Abhishek Pratap <abhishek.vit@gmail.com>
2014-09-23 22:41:02 -0700

    wrapper function included expHeatMap

--- expression_heatmap.R
+++ expression_heatmap.R
@@ -780,3 +780,38 @@ memoised_pheatmap = function(mat, color = colorRampPalette(rev(brewer.pal(n = 7,
   #return(mat)
   
 }
+
+
+#######################
+#Main Wrapper function
+#######################
+expHeatMap <- function(m, annotation = NA ,
+                       clustering_distance_rows = "correlation",
+                       clustering_distance_cols = "correlation",
+                       cor_method="spearman",
+                       clustering_method = "average",
+                       scale = FALSE,...){
+  if(nrow(m) <= 2){
+    return(memoised_pheatmap(m, cluster_rows=FALSE,
+                             scale="none",
+                             annotation = annotation,
+                             drawRowD = FALSE,
+                             border_color = NA,...))
+  }
+  else{
+    #do the clustering and heatmap
+    #scaling genes across experiments
+    if(scale == "TRUE"){
+      m <- t(scale(t(m)))
+    }
+    memoised_pheatmap(m,
+                      scale="none",
+                      annotation = annotation,
+                      clustering_distance_rows = clustering_distance_rows,
+                      clustering_distance_cols = clustering_distance_cols,
+                      clustering_method = clustering_method,
+                      border_color = NA,
+                      drawRowD = FALSE,
+                      cor_method=cor_method,...)
+  }
+}
