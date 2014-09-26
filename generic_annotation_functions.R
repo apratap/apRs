d6f7c918767c2c805f70ceb0d46d93bf52be6932
Abhishek Pratap <abhishek.vit@gmail.com>
2014-09-26 14:33:24 -0700

    added function to convert geneIds to EntrezIDs

--- generic_annotation_functions.R
+++ generic_annotation_functions.R
@@ -31,3 +31,11 @@ convert_to_HUGOIds <- function(genes){
   na.omit(unique(c(filtered_df$SYMBOL)))
 }
 
+
+#get the ENTREZ ids for the selected genes
+convert_to_EntrezIds <- function(genes){
+  filtered_df <- hg19_annot %>%
+    filter(SYMBOL %in% genes  | ENSEMBL %in% genes |
+             ENSEMBLTRANS %in% genes  | ENTREZID %in% genes )
+  na.omit(unique(c(filtered_df$ENTREZID)))
+}
